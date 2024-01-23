3760efd996a1f7abe9c3b011666c1495786cc7f2
Toan Nguyen <nguyenvantoan263@gmail.com>
2024-01-23 13:03:35 +0800

    update public access point of frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae24685757cfe4ef58f6067ec52810cb-592057546.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
